1dbc8f7411e3f7af3697b8de384e0ff4b3588b4c
DESKTOP-EN9QKNO\zcluo <zc.luo@qq.com>
2018-11-26 20:28:37 +0800

    左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ def run_game():
 
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     while True:
         gf.check_events(ship)
         ship.update()
