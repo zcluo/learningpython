4e7470e85e97a50303ec01e73a194522dd02469e
DESKTOP-EN9QKNO\zcluo <zc.luo@qq.com>
2018-12-11 16:31:40 +0800

    增加得分

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,11 @@
 import sys
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
+from alien import Alien
 from pygame.sprite import Group
+
 import game_functions as gf
 
 
@@ -13,13 +16,20 @@ def run_game():
 
     pygame.display.set_caption("Alien Invasion")
 
+    stats = GameStats(ai_settings)
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -5,8 +5,14 @@ class Settings():
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
 
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
-        self.bullets_allowed = 3
+        self.bullets_allowed = 15
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #1向右 -1向左
+        self.fleet_direction = 1
+
+        self.ship_limit = 3
